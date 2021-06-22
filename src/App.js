dd8e20a77bc650c1028f747244e82226a915de45
Guilherme Miranda <guilherme.miranda@conexasaude.com.br>
2021-06-21 22:49:42 -0300

    Aula 2

--- src/App.js
+++ src/App.js
@@ -1,25 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import Home from "./telas/Home";
+import Sidebar from "./componentes/Sidebar";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default function App() {
+	return (
+		<Router>
+			<div>
+				<Sidebar />
+				<Switch>
+					{/* <Route path="/about">
+						<About />
+					</Route>
+					<Route path="/users">
+						<Users />
+					</Route> */}
+					<Route path="/">
+						<Home />
+					</Route>
+				</Switch>
+			</div>
+		</Router>
+	);
 }
-
-export default App;
